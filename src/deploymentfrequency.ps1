b9f832a17284234bbda9e74a5dda7b3b1d86b586
André Bering <eberdna@github.com>
2022-09-15 15:12:44 +0200

    added helper functions

--- src/deploymentfrequency.ps1
+++ src/deploymentfrequency.ps1
@@ -234,4 +234,62 @@ function GetAuthHeader ([string] $patToken, [string] $actionsToken)
     return $authHeader
 }
 
+function ConvertTo-Base64UrlString([string] $in) 
+{
+    if ($in -is [string]) {
+        return [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($in)) -replace '\+','-' -replace '/','_' -replace '='
+    }
+    elseif ($in -is [byte[]]) {
+        return [Convert]::ToBase64String($in) -replace '\+','-' -replace '/','_' -replace '='
+    }
+    else {
+        throw "ConvertTo-Base64UrlString requires string or byte array input, received $($in.GetType())"
+    }
+}
+
+function Get-JwtToken([string]$appId, [string] $appInstallationId, [string] $privateKey)
+{
+    # Write-Host "appId: $appId"
+    $now = (Get-Date).ToUniversalTime()
+    $createDate = [Math]::Floor([decimal](Get-Date($now) -UFormat "%s"))
+    $expiryDate = [Math]::Floor([decimal](Get-Date($now.AddMinutes(4)) -UFormat "%s"))
+    $rawclaims = [Ordered]@{
+        iat = [int]$createDate
+        exp = [int]$expiryDate
+        iss = $appId
+    } | ConvertTo-Json
+    # Write-Host "expiryDate: $expiryDate"
+    # Write-Host "rawclaims: $rawclaims"
+
+    $Header = [Ordered]@{
+        alg = "RS256"
+        typ = "JWT"
+    } | ConvertTo-Json
+    # Write-Host "Header: $Header"
+    $base64Header = ConvertTo-Base64UrlString $Header
+    # Write-Host "base64Header: $base64Header"
+    $base64Payload = ConvertTo-Base64UrlString $rawclaims
+    # Write-Host "base64Payload: $base64Payload"
+
+    $jwt = $base64Header + '.' + $base64Payload
+    $toSign = [System.Text.Encoding]::UTF8.GetBytes($jwt)
+
+    $rsa = [System.Security.Cryptography.RSA]::Create();    
+    # https://stackoverflow.com/a/70132607 lead to the right import
+    $rsa.ImportRSAPrivateKey([System.Convert]::FromBase64String($privateKey), [ref] $null);
+
+    try { $sig = ConvertTo-Base64UrlString $rsa.SignData($toSign,[Security.Cryptography.HashAlgorithmName]::SHA256,[Security.Cryptography.RSASignaturePadding]::Pkcs1) }
+    catch { throw New-Object System.Exception -ArgumentList ("Signing with SHA256 and Pkcs1 padding failed using private key $($rsa): $_", $_.Exception) }
+    $jwt = $jwt + '.' + $sig
+    # send headers
+    $uri = "https://api.github.com/app/installations/$appInstallationId/access_tokens"
+    $jwtHeader = @{
+        Accept = "application/vnd.github+json"
+        Authorization = "Bearer $jwt"
+    }
+    $tokenRespone = Invoke-RestMethod -Uri $uri -Headers $jwtHeader -Method Post -ErrorAction Stop
+    # Write-Host $tokenRespone.token
+    return $tokenRespone.token
+}
+
 main -ownerRepo $ownerRepo -workflows $workflows -branch $branch -numberOfDays $numberOfDays -patToken $patToken -actionsToken $actionsToken

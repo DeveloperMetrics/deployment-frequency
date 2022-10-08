19b2b3c8e97c9945b1b91c9572f90711132a5444
Sam Smith <samsmithnz@github.com>
2022-10-08 14:10:25 +0000

    Added owner/repo name to output

--- src/deploymentfrequency.ps1
+++ src/deploymentfrequency.ps1
@@ -243,7 +243,7 @@ function Main ([string] $ownerRepo,
     if ($dateList.Count -gt 0 -and $numberOfDays -gt 0)
     {
         Write-Output "Deployment frequency over last $numberOfDays days, is $displayMetric $displayUnit, with a DORA rating of '$rating'"
-        return Format-OutputMarkdown -workflowNames $workflowNames -displayMetric $displayMetric -displayUnit $displayUnit -branch $branch -numberOfDays $numberOfDays -numberOfUniqueDates $uniqueDates.Length.ToString() -color $color -rating $rating
+        return Format-OutputMarkdown -workflowNames $workflowNames -displayMetric $displayMetric -displayUnit $displayUnit -repo $ownerRepo -branch $branch -numberOfDays $numberOfDays -numberOfUniqueDates $uniqueDates.Length.ToString() -color $color -rating $rating
     }
     else
     {
@@ -363,14 +363,14 @@ function Get-JwtToken([string] $appId, [string] $appInstallationId, [string] $ap
 }
 
 # Format output for deployment frequency in markdown
-function Format-OutputMarkdown([array] $workflowNames, [string] $rating, [string] $displayMetric, [string] $displayUnit, [string] $branch, [string] $numberOfDays, [string] $numberOfUniqueDates, [string] $color)
+function Format-OutputMarkdown([array] $workflowNames, [string] $rating, [string] $displayMetric, [string] $displayUnit, [string] $repo, [string] $branch, [string] $numberOfDays, [string] $numberOfUniqueDates, [string] $color)
 {
     $encodedDeploymentFrequency = [uri]::EscapeUriString($displayMetric + " " + $displayUnit)
 
     $markdown = "## DORA Metric: Deployment Frequency`r`n" +
     "![Deployment Frequency](https://img.shields.io/badge/frequency-" + $encodedDeploymentFrequency + "-" + $color + "?logo=github&label=Deployment%20frequency)`r`n" +
     "**Definition:** For the primary application or service, how often is it successfully deployed to production.`n" +
-    "**Results:** Deployment frequency for **repo** repo, **$branch** branch, over last **$numberOfDays days** is **$displayMetric $displayUnit**, with a rating of **$rating**.`n" +
+    "**Results:** Deployment frequency for **$repo** repo, **$branch** branch, over last **$numberOfDays days** is **$displayMetric $displayUnit**, with a rating of **$rating**.`n" +
     "- Workflow(s) used: $($workflowNames -join ", ")`n" +
     "- Active days of deployment: $numberOfUniqueDates days`n" + 
     "---"
